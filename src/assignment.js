23e4092fab20e0b3eaeab8e68986ed3bfc8f1f8a
PreyeOmusuku <104953950+PreyeO@users.noreply.github.com>
2022-07-04 10:03:34 +0000

    i looked for both the sum and even numbers of the array. i also removed the comments from both assignment.sumOfNumbers and assignment.countEvenNumbers

--- src/assignment.js
+++ src/assignment.js
@@ -3,7 +3,6 @@
 const assignment = {};
 // ========================
 
-
 /**
  * Challenge - 1
  * 
@@ -12,11 +11,15 @@ const assignment = {};
  * 
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
- */
+ */ 
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+   var sum=0;
+  for(index=0; index<arrayOfNumbers.length; index+=1){
+    sum=sum+arrayOfNumbers[index];
+  }
+ return (sum);
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +31,15 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  var evenNumbers=0;
+  for(index=0; index< arrayOfNumbers.length; index++) {
+    if(arrayOfNumbers[index] %2==0) {
+      evenNumbers++;
+    }
+  }
+    return evenNumbers;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
